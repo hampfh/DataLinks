00c0e7e78f2358c468ba1ed48bbc4deebcbe23cd
Hampus Hallkivst <hampus@hallkvist.org>
2020-09-02 19:40:56 +0200

    Add appearance animation when subject is rendered on load

--- client/src/components/screens/Subjects/components/Subject.tsx
+++ client/src/components/screens/Subjects/components/Subject.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import "./Subject.css"
+import "./Animation.css"
 import { SubjectData, Group, ContentObject } from '../Subjects'
 import { v4 as uuid } from "uuid"
 
@@ -9,26 +10,23 @@ export class Subject extends Component<PropsForComponent, StateForComponent> {
 		super(props)
 
 		this.state = {
-			collapsState: localStorage.getItem(this.props.subject.title) == null ? 0 : 2,
+			collapsState: localStorage.getItem(this.props.subject.title) == null ? 0 : 3,
 			subject: props.subject
 		}
 	}
 
 	_onSubjectClick = () => {
+		// Extend the subject container
 		if (this.state.collapsState === 0) {
 			const newState = { ...this.state }
-			newState.collapsState = 1
+			newState.collapsState = 3
 			this.setState(newState)
 
-			setTimeout(() => {
-				const newState = { ...this.state }
-				newState.collapsState = 2
-				this.setState(newState)
-				
-				if (this.state.subject !== undefined) 
-					localStorage.setItem(this.state.subject?.title, "1")
-			}, 200)
-		} else if (this.state.collapsState === 2) {
+			// Add subject to localstorage
+			if (this.state.subject !== undefined)
+				localStorage.setItem(this.state.subject?.title, "1")
+		// Collapse the subject container
+		} else if (this.state.collapsState === 3) {
 			const newState = { ...this.state }
 			newState.collapsState = 0
 			this.setState(newState)
@@ -57,9 +55,8 @@ export class Subject extends Component<PropsForComponent, StateForComponent> {
 					<p className="textObject">{object.text}</p>
 				}
 			</div>
-		} else {
+		} else
 			return null
-		}
 	}
 
 	render() {
@@ -67,7 +64,7 @@ export class Subject extends Component<PropsForComponent, StateForComponent> {
 			return null
 
 		return (
-			<div className="Subject" style={{ backgroundColor: this.state.subject.color }}>
+			<div className={"Subject"} style={{ backgroundColor: this.state.subject.color }}>
 				<h4 className="Header" onClick={this._onSubjectClick}>{this.state.subject.title}</h4>
 				{this.state.collapsState === 0 ? null :
 					<section className="expandable" style={{
