bd5988336833c2673766b7ad254e0f4097431b18
Hampfh <hampus@hallkvist.org>
2020-10-05 21:35:03 +0200

    Fix bugs and styleing

--- app/models/index.model.ts
+++ app/models/index.model.ts
@@ -8,14 +8,14 @@ export function connectDB(): Promise<void> {
 	// eslint-disable-next-line no-async-promise-executor
 	return new Promise<void>(async (resolve, reject) => {
 
-		mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?authSource=admin`, {
+		mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?authSource=datasektionen`, {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
 			useFindAndModify: false,
 			useCreateIndex: true,
 			auth: process.env.NODE_ENV !== "production" ? undefined : {
 				user: process.env.DB_USER as string,
-				password: process.env.DB_PASSWORD as string
+				password: process.env.DB_PASS as string
 			}
 		})
 
