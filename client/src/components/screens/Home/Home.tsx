6246011a110bcd5ada838fedcb17273150627675
Hampus Hallkvist <hampus@hallkvist.org>
2021-09-04 13:38:17 +0200

    [CLIENT] Implementation of home screen

--- client/src/components/screens/Home/Home.tsx
+++ client/src/components/screens/Home/Home.tsx
@@ -1,10 +1,96 @@
 import React from 'react'
+import "./Home.css"
+import { ReactComponent as FirstFloatingBox } from "assets/svgs/illustrations/Floatingbox#1.svg"
+import { ReactComponent as SecondFloatingBox } from "assets/svgs/illustrations/Floatingbox#2.svg"
+import { ReactComponent as ThirdFloatingBox } from "assets/svgs/illustrations/Floatingbox#3.svg"
+import { ReactComponent as Deadline } from "assets/svgs/illustrations/Deadline.svg"
+import { ReactComponent as Collaborate } from "assets/svgs/illustrations/undraw_collaborators_prrw.svg"
+import { ReactComponent as CrowdSource } from "assets/svgs/illustrations/undraw_community_8nwl.svg"
+import Background from './components/Background'
+
+import { ReactComponent as PathOne } from "assets/svgs/background/Path100.svg"
+import { ReactComponent as PathTwo } from "assets/svgs/background/Path101.svg"
+import { ReactComponent as PathThree } from "assets/svgs/background/Path102.svg"
+import { Link } from 'react-router-dom'
 
 export default function Home() {
     return (
-        <div>
-            <h1>Welcome to data links</h1>
-            <p>As you can se this screen is not very developed at the moment, try to access a specific program such as <a href="/D20">D20</a> or <a href={"/D21"}>D21</a></p>
-        </div>
+        <section>
+            <div className="home-screen-container">
+                <div className="background-container"><Background /></div>
+                <div className="homehero">
+                    <div className="homehero-title-container">
+                        <h1 className="homehero-title">Datasektionen.link</h1>
+                        <h2 className="homehero-subtitle">Stay organized with crowdsourcing</h2>
+                    </div>
+                    <div className="box-container">
+                        <FirstFloatingBox className="box" />
+                        <SecondFloatingBox className="box" />
+                        <ThirdFloatingBox className="box" />
+                    </div>
+                </div>
+                <div className="path-wrapper">
+                    <div className="short-info-container">
+                        <div className="info-container">
+                            <h3>Collaborate</h3>
+                            <Collaborate className="info-image" />
+                            <p>Create a space together with everyone else in your program where all data is neatly organized</p>
+                        </div>
+                        <div className="info-container">
+                            <h3>Crowdsourced</h3>
+                            <CrowdSource className="info-image" />
+                            <p>Let anyone perform edits to your board. Fast accessibility leads to more contributions!</p>
+                        </div>
+                        <div className="info-container">
+                            <h3>Clear deadlines</h3>
+                            <Deadline className="info-image" />
+                            <p>All deadlines are easily accessible and sorted by urgency</p>
+                        </div>
+                    </div>
+                    <div className=".path-wrapper">
+                        <PathOne className="path-one path" />
+                        <PathTwo className="path-two path" />
+                        <PathThree className="path-three path" />
+                    </div>
+                </div>
+                
+                <div className="join-too">
+                    <div className="join-too-content-container">
+                        <h3>Join one of the crowdsourced programs</h3>
+                        <div className="join-too-program-wrapper">
+                            <Link to="/D20">
+                                <div className="program-link">
+                                    <p>D20</p>
+                                </div>
+                            </Link>
+                            <Link to="/D21">
+                                <div className="program-link">
+                                    <p>D21</p>
+                                </div>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <footer className="homepage-footer">
+                <div>
+                    <h2>Datasektionen.link</h2>
+                </div>
+                <div className="homepage-footer-column-container">
+                    <div>
+                        <h3>Development</h3>
+                        <hr />
+                        <p><a className="footer-link" href="https://github.com/Hampfh/DataLinks">Contribute to the project</a></p>
+                        <p><a className="footer-link" href="https://github.com/Hampfh/DataLinks">View the source code</a></p>
+                        <p><a className="footer-link" href="https://github.com/Hampfh/DataLinks/issues">Report a bug</a></p>
+                    </div>
+                    <div>
+                        <h3>Reach out</h3>
+                        <hr />
+                        <p><a className="footer-link" href="https://datasektionen.se/">datasektionen.se</a></p>
+                    </div>
+                </div>
+            </footer>
+        </section>
     )
 }
---
+++ client/src/components/screens/Home/components/Background.tsx
@@ -0,0 +1,106 @@
+import { useEffect, useRef, useState } from 'react'
+import "./Background.css"
+
+export default function Background() {
+
+    const dotSize = 4
+    const dotMargin = 24
+    const dotInitialAlpha = 0.20
+
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth - dotMargin)
+    const [windowHeight, setWindowHeight] = useState(window.innerHeight - dotMargin)
+
+    let debouncer: NodeJS.Timeout | undefined = undefined
+    
+    const getHorizontalDotCount = () => Math.floor(windowWidth / (dotSize + dotMargin))
+    const getVerticalDotCount = () => Math.floor(windowHeight / (dotSize + dotMargin))
+    const getPaddingHorizontal = () => windowWidth - (getHorizontalDotCount() * dotSize + (getHorizontalDotCount() - 1) * dotMargin)
+    const getPaddingVertical = () => windowHeight - (getVerticalDotCount() * dotSize + (getVerticalDotCount() - 1) * dotMargin)
+    
+    const canvasRef = useRef(null)
+    
+    const [dots, setDots] = useState<Array<Array<number>>>(getDotArray())
+
+    useEffect(() => {
+
+        window.addEventListener("resize", onResize)
+
+        draw()
+        return () => {
+            window.removeEventListener("resize", onResize)
+        }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+    function getDotArray() {
+        const dots: Array<Array<number>> = []
+
+        for (let y = 0; y < getVerticalDotCount(); y++) {
+            dots.push([])
+            for (let x = 0; x < getHorizontalDotCount(); x++) {
+                dots[y].push(dotInitialAlpha)               
+            }
+        }
+        console.log("GNERATTING NEW ARRAY", dots[0].length, dots.length)
+        return dots
+    }
+
+    function onResize() {
+        const debounceDelay = 500
+
+        if (debouncer)
+            clearTimeout(debouncer)
+        
+        debouncer = setTimeout(() => {
+            console.log(window.innerWidth, window.innerHeight)
+            setWindowWidth(window.innerWidth - dotMargin)
+            setWindowHeight(window.innerHeight - dotMargin)
+
+            //setDots(getDotArray())
+            setTimeout(draw)
+        }, debounceDelay)
+    }
+
+    function draw() {
+        console.log("DRAW")
+        const canvas = canvasRef.current as HTMLCanvasElement | null
+        const context = canvas?.getContext('2d')
+
+        if (canvas == null || context == null) {
+            return
+        }
+        
+        context.save()
+        context.clearRect(0, 0, windowWidth, windowHeight)
+        context.scale(devicePixelRatio, devicePixelRatio)
+
+        for (let y = 0; y < dots.length; y++) {
+            for (let x = 0; x < dots[y].length; x++) {
+
+                context.fillStyle = `rgba(255,255,255,${dots[y][x]})`
+
+                context.beginPath()
+                context.arc(
+                    getPaddingHorizontal() / 2 + (dotSize + dotMargin) * x, 
+                    getPaddingVertical() / 2 + (dotSize + dotMargin) * y,
+                    dotSize / 2, 
+                    0, 
+                    Math.PI * 2
+                )
+                context.fill();
+            }
+        }
+        context.restore()
+    }
+    
+    return <canvas 
+        className="canvas" 
+        ref={canvasRef} 
+        width={windowWidth * devicePixelRatio} 
+        height={windowHeight * devicePixelRatio} 
+        style={{
+            width: windowWidth,
+            height: windowHeight
+        }} 
+    />
+}
