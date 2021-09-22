ff28131ed530882a98aa76855dd3c5bd67aacfbc
Hampus Hallkvist <hampus@hallkvist.org>
2021-09-22 12:13:42 +0200

    [CLIENT] Hide program when in contribution page This is because the contributions isn't actually specific to the program Instead they are global for the whole site, thus it doesn't really make sense to have the program name on the top of the page

--- client/src/components/templates/headers/components/HeaderPagePresenter.tsx
+++ client/src/components/templates/headers/components/HeaderPagePresenter.tsx
@@ -4,7 +4,11 @@ import "./HeaderPagePresenter.css"
 export default function HeaderPagePresenter(props: PropsForComponent) {
     return (
         <div className="header-page-presenter-container">
-            <h2 className="header-page-presenter-primary">{props.program}'s</h2>
+            { // TODO This is a quick fix, do not display program when in contribution page (since this is not actually specific to the program but to the site)
+            props.pagePresenter === "Contributors" ?
+                <h2 className="header-page-presenter-primary">DataLinks</h2> :
+                <h2 className="header-page-presenter-primary">{props.program}'s</h2>
+            }
             <h3 className="header-page-presenter-secondary">{props.pagePresenter}</h3>
         </div>
     )
